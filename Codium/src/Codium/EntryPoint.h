bd670d0d94e6a5efd20bea3425f384cce919394d
An1n.dya <74007285+an1n-dya@users.noreply.github.com>
2025-01-19 21:51:50 +0600

    Added premake5

--- Codium/src/Codium/EntryPoint.h
+++ Codium/src/Codium/EntryPoint.h
@@ -6,9 +6,7 @@ extern Codium::Application* Codium::CreateApplication();
 
 int main(int argc, char** argv) {
 	Codium::Log::Init();
-	CE_CORE_WARN("Initialized Log!");
-	int a = 5;
-	CE_INFO("Hello! Var={0}", a);
+	CE_CORE_WARN("Logging Initialized");
 
 	auto app = Codium::CreateApplication();
 	app->Run();
---
+++ premake5.lua
@@ -0,0 +1,104 @@
+workspace "Codium"
+	architecture "x64"
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+startproject "Sandbox"
+
+project "Codium"
+	location "Codium"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		buildoptions { "/utf-8" }
+
+		defines {
+			"CE_PLATFORM_WINDOWS",
+			"CE_BUILD_DLL"
+		}
+
+		postbuildcommands {
+			"{MKDIR} ../bin/" .. outputdir .. "/Sandbox",
+    		"{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox/"
+		}
+
+	filter "configurations:Debug"
+		defines "CE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CE_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"Codium/vendor/spdlog/include",
+		"Codium/src"
+	}
+
+	links {
+		"Codium"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		buildoptions { "/utf-8" }
+
+		defines {
+			"CE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "CE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CE_DIST"
+		optimize "On"
+        
