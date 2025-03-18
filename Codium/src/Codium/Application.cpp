dc8c0ba1dfef3923d54edfff80b7060dd669024b
An1n.dya <LetsACode@outlook.com>
2025-03-18 10:00:13 +0600

    Dispatching events from GLFW

--- Codium/src/Codium/Application.cpp
+++ Codium/src/Codium/Application.cpp
@@ -1,17 +1,26 @@
 #include "cepch.h"
 #include "Application.h"
 
-#include "Codium/Events/ApplicationEvent.h"
 #include "Codium/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Codium {
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+	
 	Application::Application() {
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application() { }
+
+	void Application::OnEvent(Event& e)	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		
+		CE_CORE_TRACE("{0}", e);
+	}
 
 	void Application::Run() {
 		while (m_Running) {
@@ -19,5 +28,10 @@ namespace Codium {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e) {
+		m_Running = false;
+		return true;
 	}
 }
