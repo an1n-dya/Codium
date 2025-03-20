f7c084e21ad8208733277b40b056cb4dd959f489
An1n.dya <74007285+an1n-dya@users.noreply.github.com>
2025-03-20 16:27:12 +0600

    Added ImGui and basic ImGui layer (Part 3)

--- Codium/src/Codium/ImGui/ImGuiLayer.cpp
+++ Codium/src/Codium/ImGui/ImGuiLayer.cpp
@@ -1,1 +1,76 @@
-//TODO: ImGuiLayer.cpp
+#include "cepch.h"
+#include "ImGuiLayer.h"
+
+#include "imgui.h"
+#include "Platform/OpenGL/ImGuiOpenGLRenderer.h"
+#include "GLFW/glfw3.h"
+
+#include "Codium/Application.h"
+
+namespace Codium {
+	ImGuiLayer::ImGuiLayer()
+		: Layer("ImGuiLayer") { }
+
+	ImGuiLayer::~ImGuiLayer() { }
+
+	void ImGuiLayer::OnAttach() {
+		ImGuiIO& io = ImGui::GetIO();
+		IMGUI_CHECKVERSION();
+			
+		io.BackendPlatformName = "imgui_impl_glfw";
+		io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;
+		io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
+		
+		m_Time = 0.0f;
+		
+		Application& app = Application::Get();
+		
+		// Need to be fixed :
+		// GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetNativeWindow());
+		
+		GLFWerrorfun prev_error_callback = glfwSetErrorCallback(nullptr);
+		m_MouseCursors[ImGuiMouseCursor_Arrow] = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
+		m_MouseCursors[ImGuiMouseCursor_TextInput] = glfwCreateStandardCursor(GLFW_IBEAM_CURSOR);
+		m_MouseCursors[ImGuiMouseCursor_ResizeNS] = glfwCreateStandardCursor(GLFW_VRESIZE_CURSOR);
+		m_MouseCursors[ImGuiMouseCursor_ResizeEW] = glfwCreateStandardCursor(GLFW_HRESIZE_CURSOR);
+		m_MouseCursors[ImGuiMouseCursor_Hand] = glfwCreateStandardCursor(GLFW_HAND_CURSOR);
+	#if GLFW_HAS_NEW_CURSORS
+		m_MouseCursors[ImGuiMouseCursor_ResizeAll] = glfwCreateStandardCursor(GLFW_RESIZE_ALL_CURSOR);
+		m_MouseCursors[ImGuiMouseCursor_ResizeNESW] = glfwCreateStandardCursor(GLFW_RESIZE_NESW_CURSOR);
+		m_MouseCursors[ImGuiMouseCursor_ResizeNWSE] = glfwCreateStandardCursor(GLFW_RESIZE_NWSE_CURSOR);
+		m_MouseCursors[ImGuiMouseCursor_NotAllowed] = glfwCreateStandardCursor(GLFW_NOT_ALLOWED_CURSOR);
+	#else
+		m_MouseCursors[ImGuiMouseCursor_ResizeAll] = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
+		m_MouseCursors[ImGuiMouseCursor_ResizeNESW] = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
+		m_MouseCursors[ImGuiMouseCursor_ResizeNWSE] = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
+		m_MouseCursors[ImGuiMouseCursor_NotAllowed] = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
+	#endif
+		glfwSetErrorCallback(prev_error_callback);
+		
+		ImGui::CreateContext();
+		ImGui_ImplOpenGL3_Init("#version 410");
+	}
+
+	void ImGuiLayer::OnDetach() { }
+
+	void ImGuiLayer::OnUpdate() {
+		ImGuiIO& io = ImGui::GetIO();
+		Application& app = Application::Get();
+		io.DisplaySize = ImVec2(app.GetWindow().GetWidth(), app.GetWindow().GetHeight());
+
+		float time = (float)glfwGetTime();
+		io.DeltaTime = m_Time > 0.0f ? (time - m_Time) : (1.0f / 60.0f);
+		m_Time = time;
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui::NewFrame();
+
+		static bool show = true;
+		ImGui::ShowDemoWindow(&show);
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	}
+
+	void ImGuiLayer::OnEvent(Event& event) { }
+}
--- Codium/src/Codium/ImGui/ImGuiLayer.h
+++ Codium/src/Codium/ImGui/ImGuiLayer.h
@@ -1,1 +1,23 @@
-//TODO: ImGuiLayer.h
+#pragma once
+
+#include "Codium/Layer.h"
+
+#include "imgui.h"
+
+struct GLFWcursor;
+
+namespace Codium {
+    class CODIUM_API ImGuiLayer : public Layer {
+    public:
+        ImGuiLayer();
+        ~ImGuiLayer();
+
+        void OnAttach();
+        void OnDetach();
+        void OnUpdate();
+        void OnEvent(Event& event);
+    private:
+        float m_Time = 0.0f;
+        GLFWcursor* m_MouseCursors[ImGuiMouseCursor_COUNT] = {};
+    };
+}
