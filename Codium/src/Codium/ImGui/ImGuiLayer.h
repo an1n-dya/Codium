4d399ffba6b035cf2520d9679368d90bed6def77
An1n.dya <74007285+an1n-dya@users.noreply.github.com>
2025-03-21 05:04:53 +0600

    Added events to ImGui layer (Part 1)

--- Codium/src/Codium/ImGui/ImGuiLayer.h
+++ Codium/src/Codium/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Codium/Layer.h"
 
+#include "Codium/Events/ApplicationEvent.h"
+#include "Codium/Events/KeyEvent.h"
+#include "Codium/Events/MouseEvent.h"
+
 namespace Codium {
 	class CODIUM_API ImGuiLayer : public Layer {
 	public:
@@ -12,6 +16,15 @@ namespace Codium {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
