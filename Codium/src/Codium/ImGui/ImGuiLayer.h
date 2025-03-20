cf4a016f1ddcad071d3d83c1855f12e435926088
An1n.dya <74007285+an1n-dya@users.noreply.github.com>
2025-03-20 17:45:06 +0600

    BACKTRACK (ImGUI)

--- Codium/src/Codium/ImGui/ImGuiLayer.h
+++ Codium/src/Codium/ImGui/ImGuiLayer.h
@@ -2,22 +2,17 @@
 
 #include "Codium/Layer.h"
 
-#include "imgui.h"
-
-struct GLFWcursor;
-
 namespace Codium {
-    class CODIUM_API ImGuiLayer : public Layer {
-    public:
-        ImGuiLayer();
-        ~ImGuiLayer();
+	class CODIUM_API ImGuiLayer : public Layer {
+	public:
+		ImGuiLayer();
+		~ImGuiLayer();
 
-        void OnAttach();
-        void OnDetach();
-        void OnUpdate();
-        void OnEvent(Event& event);
-    private:
-        float m_Time = 0.0f;
-        GLFWcursor* m_MouseCursors[ImGuiMouseCursor_COUNT] = {};
-    };
+		void OnAttach();
+		void OnDetach();
+		void OnUpdate();
+		void OnEvent(Event& event);
+	private:
+		float m_Time = 0.0f;
+	};
 }
